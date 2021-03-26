a7f8d6e30e0da076ad57f80531c37f8d467f430a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-26 14:10:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dbstart
+ 
---
+++ src/main/java/com/db/DbstartApplication.java
@@ -0,0 +1,13 @@
+package com.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbstartApplication.class, args);
+	}
+
+}
